3898ed0e3e2825ee2f0f132e2b26183ae1882bc3
Magalhaexz <magalhaesh617@gmail.com>
2026-04-21 22:57:13 -0300

    Resolve comparativo merge conflicts and fix PesagemForm UI imports

--- src/components/PesagemForm.jsx
+++ src/components/PesagemForm.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
-import Modal from '../ui/Modal';
-import Button from '../ui/Button';
+import Modal from './ui/Modal';
+import Button from './ui/Button';
 import ArrobaPreview from './ArrobaPreview';
 
 const FORM_VAZIO = {
